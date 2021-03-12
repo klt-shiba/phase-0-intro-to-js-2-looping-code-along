a681e33aabce3578e0a5aa0cef419f23d41d24a9
Colin Tseng <klt.aus@yahoo.com>
2021-03-12 20:31:43 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const person = [ 'Lisa', 'Kaitlin', 'Jan'];
+
+
+
+function writeCards(person) {
+
+  const personArray = [];
+
+  for (let i = 0; i < person.length; i++) {
+    personArray.push(`Thank you, ${person[i]}, for the wonderful surprise gift!`);
+  }
+  return personArray;
+}
+
+writeCards(person);
+
+
+function countDown(number) {
+    while (number >= 0) {
+
+        console.log(number--);
+        // number--
+      }
+      return "All done";
+}
+countDown(10);
+
+
+
